022020e4fc075dff0c6bf410c4739cf7e3dc4080
kareem <kareemmorsy555@gmail.com>
2024-11-19 23:45:19 +0200

    feat:update user profile page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import React from 'react'
-import { Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import User from './pages/User'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React, { lazy, Suspense } from 'react'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+
+
+const Home = lazy(() => import('./pages/Home'));
+const User = lazy(() => import('./pages/User'));
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+})
 
 function App() {
   return (
-    
-    <Routes>
-      <Route path='/'  element={<Home/>}/>
-      <Route path='/edit-user/:id' element={<User/>}/>
-
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/edit-user/:id' element={<User />} />
+      </Routes>
+    </Suspense>
   )
 }
 
-export default App
+export default () => (  
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
+)
